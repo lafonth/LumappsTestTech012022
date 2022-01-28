4fc7b184b34edf551d9876d2decf46c0f9f30874
lafonth <lafonthub@gmail.com>
2022-01-28 22:10:11 +0100

    header clean up

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,10 +5,10 @@ import SearchField from '../SearchField';
 
 const Header = () => (
 	<header className="lumx-spacing-padding-big header">
-		<NavLink to="/">
-			<img className="marvel" src="/Marvel_Logo.svg" alt="Marvel Logo" />
-		</NavLink>
-		<FlexBox vAlign={Alignment.right}>
+		<FlexBox wrap="false" className="spaceBetween">
+			<NavLink to="/">
+				<img className="marvel" src="/Marvel_Logo.svg" alt="Marvel Logo" />
+			</NavLink>
 			<SearchField />
 		</FlexBox>
 	</header>
--- src/components/SearchField/index.jsx
+++ src/components/SearchField/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
-import { TextField, Theme } from '@lumx/react';
+import React, { useState } from 'react';
+import {
+  TextField, Theme, Icon, Size, FlexBox, Button, Emphasis,
+} from '@lumx/react';
 import { mdiMagnify } from '@lumx/icons';
 
-const SearchField = () => (
-	<TextField theme={Theme.dark} placeholder="Search ..." icon={mdiMagnify} />
-);
+const SearchField = () => {
+  const [inputString, setInputString] = useState('');
+
+  const searchItems = (searchString) => {
+    console.log(searchString);
+    // alert(`Search:${searchString.toString()}`);
+  };
+
+  return (
+	<FlexBox wrap="False">
+		<Button theme={Theme.dark} emphasis={Emphasis.low} onClick={searchItems(inputString)}>
+			<Icon icon={mdiMagnify} size={Size.m} />
+		</Button>
+		<TextField
+			clearButtonProps={{ label: 'Clear' }}
+			theme={Theme.dark}
+			placeholder="Search..."
+			onKeyPress={(e) => e.key === 'Enter' && searchItems(inputString)}
+			onChange={(e) => setInputString(e)}
+		/>
+	</FlexBox>
+
+  );
+};
 
 export default SearchField;
